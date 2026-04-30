60e15f4cd07a7222974a2bad727fead465d5d47e
Bluewitcherindia <rasvikas124verma@gmail.com>
2026-04-30 11:05:28 +0530

    upafdted

--- web/auth.config.ts
+++ web/auth.config.ts
@@ -2,6 +2,7 @@ import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
   secret: process.env.NEXTAUTH_SECRET,
+  trustHost: true,
   providers: [],
   pages: { signIn: "/login" },
   session: { strategy: "jwt" },
